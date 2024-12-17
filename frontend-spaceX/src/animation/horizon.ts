55d991978b219281cdbafccf923c42e12146eea7
Tetsu.Kuribayashi <tktkchestnut[gmail.com>
2024-12-17 20:04:58 +0900

    :wrench: (frontend) fixed horizon settings

--- frontend-spaceX/src/animation/horizon.ts
+++ frontend-spaceX/src/animation/horizon.ts
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 
 export function createHorizon(): THREE.Mesh {
-    // THREE.PlaneGeometry を THREE.CircleGeometry に変更
-    const horizonGeometry = new THREE.CircleGeometry(5000, 64); // 半径5000、セグメント数64の円
+    const horizonGeometry = new THREE.CircleGeometry(5000, 64);
     const horizonMaterial = new THREE.MeshStandardMaterial({
         color: 0x2f4f4f,
         side: THREE.DoubleSide
     });
     const horizon = new THREE.Mesh(horizonGeometry, horizonMaterial);
-    horizon.rotation.x = -Math.PI / 2; // 地平線が地面に平行になるように回転
+    // 地平線が地面に平行になるように回転
+    horizon.rotation.x = -Math.PI / 2;
     horizon.receiveShadow = true;
     return horizon;
 }
